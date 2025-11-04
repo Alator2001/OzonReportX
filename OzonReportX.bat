1fe08432d0e1243ff17be1b968530cfe43b9153b
Alator2001 <56924664+Alator2001@users.noreply.github.com>
2025-11-04 20:31:47 +0300

    Update OzonReportX.bat

--- OzonReportX.bat
+++ OzonReportX.bat
@@ -4,10 +4,15 @@ cd /d "%~dp0"
 
 where python >nul 2>nul
 if errorlevel 1 (
-  echo Python не найден в PATH. Установите Python 3.10+ и попробуйте снова.
+  echo [❌] Python не найден.
+  echo Установите Python 3.10+ с сайта https://www.python.org/downloads/
+  echo и при установке обязательно отметьте "Add Python to PATH".
   pause
   exit /b 1
 )
+
+echo [✅] Python найден.
+echo Запуск программы...
 
 python "scripts\first_run_setup.py"
 pause
